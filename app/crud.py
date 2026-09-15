371b62e89d24bad5580d245a29768461ffe0a205
Yury Araujo <127779626+YuryOAraujo@users.noreply.github.com>
2026-09-15 00:16:31 -0300

    (feat) add crud operations for character.

---
+++ app/crud.py
@@ -0,0 +1,51 @@
+from typing import List
+
+from models import Character
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+class Crud():
+  def __init__(self, session: Session):
+    self.session = session
+
+  def add(self, **kwargs) -> Character:
+    character = Character(**kwargs)
+    self.session.add(character)
+    self.session.commit()
+    return character
+
+  def update(self, **kwargs) -> Character | None:
+    id = kwargs.get('id')
+
+    character = self.get(id=id)
+
+    if character is None:
+      return None
+
+    allowed_fields = {'name', 'level', 'gold'}
+
+    for key, value in kwargs.items():
+      if key in allowed_fields:
+        setattr(character, key, value)
+
+    self.session.commit()
+    return character
+
+  def delete(self, **kwargs) -> Character | None:
+    character = self.get(**kwargs)
+
+    if character:
+      self.session.delete(character)
+      self.session.commit()
+
+    return character
+
+  def list(self) -> List[Character]:
+    statement = select(Character)
+    
+    return self.session.scalars(statement).all()
+
+  def get(self, **kwargs) -> Character | None:
+    statement = select(Character).filter_by(**kwargs)
+
+    return self.session.scalars(statement).first()
--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
-from models import Base, Character
+from crud import Crud
+from models import Base
 from db import engine, SessionLocal
 
 def app():
   Base.metadata.create_all(engine)
 
   with SessionLocal() as session:
-    character = Character(
-      name='Gorak',
-      level=1,
-      gold=50
-    )
+    crud = Crud(session)
+    character = crud.add(name='Grimma', level=1, gold=100)
 
-    session.add(character)
-    session.commit()
+    print('ADD', character)
+    print('GET', crud.get(name='Grimma'))
+    print('UPDATE', crud.update(id=character.id, name='Lah Ghar', level=2, gold=50))
+    print('LIST', crud.list())
+    print('DELETE', crud.delete(id = character.id))
 
 if __name__ == '__main__':
   app()
